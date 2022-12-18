acca403bc4c5397d49afb9231c45245adf799a9b
aaku111 <aakash4455logicseed@gmail.com>
2022-12-18 13:40:01 +0545

    changes in blog screen

--- lib/screens/home/more/blog/blog.screen.dart
+++ lib/screens/home/more/blog/blog.screen.dart
@@ -127,7 +127,9 @@ class _BlogScreenState extends State<BlogScreen> {
                       },
                     );
                   }
-                  return const Text('loading');
+                  return Center(
+                    child: CircularProgressIndicator(color: Colors.red),
+                  );
                 }),
           )
         ],
