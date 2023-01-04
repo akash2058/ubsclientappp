822a1a2754addff80f7feed672ee372ab1409483
aaku111 <aakash4455logicseed@gmail.com>
2023-01-04 13:35:13 +0545

    changes in more settings

--- lib/screens/home/more/profile.screen.dart
+++ lib/screens/home/more/profile.screen.dart
@@ -1,7 +1,38 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:ubsclient/models/profilemodel.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
+
+  Future<List<ProfileModel>> postData() async {
+    try {
+      List<ProfileModel> profilelist = [];
+
+      final pref = await SharedPreferences.getInstance();
+      int user = pref.getInt('user')!;
+      final response = await http.post(
+          Uri.parse(
+              'https://api.ubs.com.np/index.php?method=get_service_seeker'),
+          body: {
+            "user": user.toString(),
+          });
+      var resposeData = jsonDecode(response.body.toString());
+      if (resposeData["response"] == "success") {
+        for (Map<String, dynamic> index in resposeData["seeker"]) {
+          profilelist.add(ProfileModel.fromMap(index));
+        }
+        return profilelist;
+      } else {
+        return profilelist;
+      }
+    } catch (err) {
+      throw Exception(err);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,157 +43,46 @@ class ProfileScreen extends StatelessWidget {
         automaticallyImplyLeading: true,
       ),
       backgroundColor: Colors.white,
-      body: Container(
-        margin: EdgeInsets.all(8.0),
-        padding: EdgeInsets.all(8.0),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              CircleAvatar(
-                backgroundImage: AssetImage(
-                  'assets/images/ee.jpg',
-                ),
-                radius: 90,
+      body: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.all(10),
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: const [
+                  SizedBox(
+                    width: 15,
+                  ),
+                ],
               ),
-              Divider(
-                color: Colors.black,
-                height: 15,
-                thickness: 1,
-                indent: 10,
-                endIndent: 10,
-              ),
-              Text(
-                'Full Name',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10)),
-                    hintText: 'Name'),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                'DATE OF BIRTH',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10)),
-                    hintText: 'Enter Your Date of birth'),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                'GENDER',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10))),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                'PERMANENT ADDRESS',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10)),
-                    hintText: 'Enter Your Permanent Address'),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                'TEMPORARY ADDRESS',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10)),
-                    hintText: 'Enter Your Temporary Address'),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                'PHONE',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10)),
-                    hintText: 'Enter your Phone Number'),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                'EMAIL',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10)),
-                    hintText: 'Enter Your Email'),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                'AFFIALTED INSTITUTE',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10)),
-                    hintText: 'Enter Your affialted institution'),
-              ),
-              OutlinedButton(
-                onPressed: () {},
-                style: OutlinedButton.styleFrom(
-                    backgroundColor: Colors.blue,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    )),
-                child: Text(
-                  'Save My Profile',
-                  style: TextStyle(color: Colors.white),
-                ),
-              )
-            ],
+            ),
           ),
-        ),
+          Expanded(
+            child: FutureBuilder(
+                future: postData(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    var data = snapshot.data!;
+                    return ListView.builder(
+                      itemCount: data.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          margin: const EdgeInsets.all(8),
+                          padding: const EdgeInsets.all(5),
+                          child: Container(
+                              decoration: const BoxDecoration(boxShadow: []),
+                              child: Text(data[index].toString())),
+                        );
+                      },
+                    );
+                  }
+                  return Center(
+                    child: CircularProgressIndicator(color: Colors.red),
+                  );
+                }),
+          )
+        ],
       ),
     );
   }
